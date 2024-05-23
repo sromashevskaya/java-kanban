45a5e2ca333eb90301e67759c42807df7efa22cb
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2024-05-23 18:36:35 +0300

    Initial commit

---
+++ src/Epic.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Epic extends Task{
+    private final ArrayList<Integer> subTaskList = new ArrayList<>();
+
+    public Epic(String name, String description) {
+        super(name, description);
+    }
+
+    public Epic(String name, String description, int taskId) {
+        super(name, description, taskId);
+    }
+
+    public ArrayList<Integer> getSubTaskList() {
+        return subTaskList;
+    }
+
+    public void removeSubTaskByEpicId(int id) {
+        subTaskList.remove(id);
+    }
+}
